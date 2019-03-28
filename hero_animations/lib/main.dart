b5653b08fb5508d7b0a6ec8993902da942cf04a3
shahrohan05 <shahrohan07@live.com>
2019-03-28 11:16:21 +0530

    hero_animations : Added simple hero animation

--- hero_animations/lib/main.dart
+++ hero_animations/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 void main() => runApp(MyApp());
 
@@ -16,6 +17,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class PhotoHero extends StatelessWidget {
+  const PhotoHero({Key key, this.photo, this.onTap, this.width}) : super(key:key);
+
+  final String photo;
+  final VoidCallback onTap;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: Hero(
+        tag: photo,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            child: Image.asset(
+              photo,
+              fit:BoxFit.contain
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
@@ -27,11 +56,41 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {   
+    timeDilation = 5.0;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center (child: Text('Sample App'))
+      body: Center (
+        child: PhotoHero(
+          photo: 'images/IMG_20180722_145506_063.jpg',
+          width: 300.0,
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: Text('Hero Image Page'),
+                  ),
+                  body: Container(
+                    color: Colors.lightBlueAccent,
+                    padding: const EdgeInsets.all(16.0),
+                    alignment: Alignment.topLeft,
+                    child: PhotoHero(
+                      photo: 'images/IMG_20180722_145506_063.jpg',
+                      width: 100.0,
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      }
+                    ),
+                  ),
+                ); 
+              }
+            ));
+          },
+        ),
+      )
     );
   }
 }
