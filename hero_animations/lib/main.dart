6fa747f3c0a2af3d55390200485cec2a8e7f10a9
shahrohan05 <shahrohan07@live.com>
2019-03-29 11:26:53 +0530

    added radial hero animation example

--- hero_animations/lib/main.dart
+++ hero_animations/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart' show timeDilation;
+//import 'package:flutter/scheduler.dart' show timeDilation;
+
+import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
@@ -18,7 +20,8 @@ class MyApp extends StatelessWidget {
 }
 
 class PhotoHero extends StatelessWidget {
-  const PhotoHero({Key key, this.photo, this.onTap, this.width}) : super(key:key);
+  const PhotoHero({Key key, this.photo, this.onTap, this.width})
+      : super(key: key);
 
   final String photo;
   final VoidCallback onTap;
@@ -34,10 +37,7 @@ class PhotoHero extends StatelessWidget {
           color: Colors.transparent,
           child: InkWell(
             onTap: onTap,
-            child: Image.asset(
-              photo,
-              fit:BoxFit.contain
-            ),
+            child: Image.asset(photo, fit: BoxFit.contain),
           ),
         ),
       ),
@@ -54,43 +54,194 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const double kMinRadius = 32.0;
+  static const double kMaxRadius = 128.0;
+  static const opacityCurve = const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn); 
+
+  static RectTween _createRectTween(Rect begin, Rect end) {
+    return MaterialRectArcTween(begin: begin, end: end);
+  }
+
+  static Widget _buildPage(BuildContext context, String imageName, String description) {
+    return Container(
+      color: Theme.of(context).canvasColor,
+      child: Center(
+        child: Card(
+          elevation: 16.0,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              SizedBox(
+                width: kMaxRadius * 3.0,
+                height: kMaxRadius * 3.0,
+                child: Hero(
+                  createRectTween: _createRectTween,
+                  tag: imageName,
+                  child: RadialExpansion(
+                    maxRadius: kMaxRadius,
+                    child: Photo(photo: imageName,onTap: () {
+                      Navigator.of(context).pop();
+                    }),
+                  ),
+                ),
+              ),
+              Text(
+                description,
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textScaleFactor: 2.0,
+              ),
+              const SizedBox(height: 16.0)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHero(BuildContext context, String imageName, String description) {
+    return Container(
+      width: kMinRadius * 2.0,
+      height: kMinRadius * 2.0,
+      child: Hero(
+        createRectTween: _createRectTween,
+        tag: imageName,
+        child: RadialExpansion(
+          maxRadius: kMaxRadius,
+          child: Photo(
+            photo:imageName,
+            onTap: () {
+              Navigator.of(context).push(
+                PageRouteBuilder(
+                  pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+                    return AnimatedBuilder(
+                      animation: animation,
+                      builder: (BuildContext context, Widget child) {
+                        return Opacity(
+                          opacity: opacityCurve.transform(animation.value),
+                          child: _buildPage(context, imageName, description),
+                        );
+                      },
+                    );
+                  }
+                )
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context) {   
-    timeDilation = 5.0;
+  Widget build(BuildContext context) {
+    //timeDilation = 5.0;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center (
-        child: PhotoHero(
-          photo: 'images/IMG_20180722_145506_063.jpg',
-          width: 300.0,
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (BuildContext context) {
-                return Scaffold(
-                  appBar: AppBar(
-                    title: Text('Hero Image Page'),
-                  ),
-                  body: Container(
-                    color: Colors.lightBlueAccent,
-                    padding: const EdgeInsets.all(16.0),
-                    alignment: Alignment.topLeft,
-                    child: PhotoHero(
-                      photo: 'images/IMG_20180722_145506_063.jpg',
-                      width: 100.0,
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      }
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+            PhotoHero(
+              photo: 'images/IMG_20180722_145506_063.jpg',
+              width: 300.0,
+              onTap: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: Text('Hero Image Page'),
                     ),
-                  ),
-                ); 
-              }
-            ));
+                    body: Container(
+                      color: Colors.lightBlueAccent,
+                      padding: const EdgeInsets.all(16.0),
+                      alignment: Alignment.topLeft,
+                      child: PhotoHero(
+                          photo: 'images/IMG_20180722_145506_063.jpg',
+                          width: 100.0,
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          }),
+                    ),
+                  );
+                }));
+              },
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              width: 300,
+              height: 250,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    _buildHero(context, 'images/1.jpg', 'Image 1'),
+                    _buildHero(context, 'images/2.jpg', 'Image 2'),
+                    _buildHero(context, 'images/3.jpg', 'Image 3')
+
+                  ],
+                ),
+              )
+            )
+          ],
+        )));
+  }
+}
+
+class Photo extends StatelessWidget {
+  Photo({Key key, this.photo,this.color,this.onTap}) :super(key:key);
+
+  final String photo;
+  final Color color;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      //color: Theme.of(context).primaryColor.withOpacity(0.25),
+      child: InkWell(
+        onTap: onTap,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints size) {
+            return Image.asset(
+              photo,
+              fit :BoxFit.contain
+            );
           },
         ),
-      )
+      ),
     );
   }
 }
+
+class RadialExpansion extends StatelessWidget {
+  RadialExpansion({
+    Key key,
+    this.maxRadius,
+    this.child
+  }) : clipRectSize = 3.0 * (maxRadius / math.sqrt2), super(key: key);
+
+  final double maxRadius;
+  final clipRectSize;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Center(
+        child: SizedBox(
+          width: clipRectSize,
+          height: clipRectSize,
+          child: ClipRect(
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
